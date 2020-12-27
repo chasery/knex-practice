fa23142d59799b7d71e55df474cab269171af751
Ryan Chase <ryan@chasery.com>
2020-12-27 12:41:23 -0800

    Add paginated page query

--- src/drills.js
+++ src/drills.js
@@ -6,14 +6,30 @@ const knexInstance = knex({
   connection: process.env.DB_URL,
 });
 
-function searchShoppingList(term) {
+function searchShoppingList(searchTerm) {
   knexInstance
     .select()
     .from("shopping_list")
-    .where("name", "ILIKE", `%${term}%`)
+    .where("name", "ILIKE", `%${searchTerm}%`)
     .then((result) => {
       console.log(result);
     });
 }
 
-searchShoppingList("burg");
+// searchShoppingList("burg");
+
+function paginateShoppingList(pageNumber) {
+  const itemsPerPage = 6;
+  const offset = itemsPerPage * (pageNumber - 1);
+
+  knexInstance
+    .select()
+    .from("shopping_list")
+    .limit(itemsPerPage)
+    .offset(offset)
+    .then((result) => {
+      console.log(result);
+    });
+}
+
+paginateShoppingList(2);
